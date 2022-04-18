66ded72a868d0a1a59b6ad7dae8f705d40687f5f
Cuong Phan <vcuong0909@gmail.com>
2022-04-18 19:20:15 -0400

    1: Created fragments UI XML

---
+++ app/src/main/java/com/offline/continentalrecipesusingnavgraph/view/EntryActivity.kt
@@ -0,0 +1,12 @@
+package com.offline.continentalrecipesusingnavgraph.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.offline.continentalrecipesusingnavgraph.R
+
+class EntryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entry)
+    }
+}
