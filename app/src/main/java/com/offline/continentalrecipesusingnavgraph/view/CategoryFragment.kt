d01bf7c00e13c541c794ea46568219bf8af38cec
Cuong Phan <vcuong0909@gmail.com>
2022-04-22 10:53:27 -0400

    2: Completed basic UI

--- app/src/main/java/com/offline/continentalrecipesusingnavgraph/view/CategoryFragment.kt
+++ app/src/main/java/com/offline/continentalrecipesusingnavgraph/view/CategoryFragment.kt
@@ -1,20 +1,30 @@
 package com.offline.continentalrecipesusingnavgraph.view
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.offline.continentalrecipesusingnavgraph.R
+import com.offline.continentalrecipesusingnavgraph.databinding.FragmentCategoryBinding
 
 
 class CategoryFragment : Fragment() {
 
+    private lateinit var binding: FragmentCategoryBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+    ): View {
+        binding = FragmentCategoryBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener{
+            findNavController().navigate(R.id.action_categoryFragment_to_mealFragment)
+        }
     }
 }
--- app/src/main/java/com/offline/continentalrecipesusingnavgraph/view/EntryActivity.kt
+++ app/src/main/java/com/offline/continentalrecipesusingnavgraph/view/EntryActivity.kt
@@ -2,11 +2,54 @@ package com.offline.continentalrecipesusingnavgraph.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
 import com.offline.continentalrecipesusingnavgraph.R
+import com.offline.continentalrecipesusingnavgraph.databinding.ActivityEntryBinding
 
 class EntryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEntryBinding
+    private lateinit var appBarConfig: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_entry)
+        binding = ActivityEntryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val host = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = host.navController
+
+        appBarConfig = AppBarConfiguration(
+            setOf(R.id.category_fragment),
+            drawerLayout = binding.drawerLayout
+        )
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBar(navController, appBarConfig)
+        setupNavigationDrawer(navController)
+    }
+
+    // display the up button if not top destination and hamburger icon if it is top destination
+    // set the action bar title based on destination's label
+    private fun setupActionBar(navController: NavController, appBarConfig: AppBarConfiguration) {
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    // if up button is clicked then go back the previous destination
+    // if the hamburger icon is clicked then display the nav view
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfig)
+    }
+
+    private fun setupNavigationDrawer(navController: NavController) {
+        binding.navView.setupWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment)) || super.onOptionsItemSelected(item)
     }
 }
